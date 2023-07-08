2a3b7bc073452e773a7aae8312049fb0e5c56cd4
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2023-07-08 21:02:56 +0530

    Greeting Spring

---
+++ src/main/java/com/example/greetingapi/GreetingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greetingapi/GreetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
