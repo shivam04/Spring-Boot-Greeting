4823090dadfb09a913b8a7a7a8205fb6a8002289
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2023-07-14 04:10:27 +0530

    JSON Response

--- src/main/java/com/example/greetingapi/hello/HelloController.java
+++ src/main/java/com/example/greetingapi/hello/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.greetingapi.hello;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HelloController {
-    @GetMapping("")
-    public String hello() {
-        return "Hello World!";
+    @GetMapping(value = "/")
+    public ResponseEntity<Object> hello() {
+        return new ResponseEntity<>(HelloDTO.builder().message("Hello World!!").build(), HttpStatus.OK);
     }
 }
